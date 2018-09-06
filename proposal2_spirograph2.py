0821cc17641c7e99e63a19dc621c79aa21789473
Rachel Lutes <rlutes@uncc.edu>
2018-09-06 16:25:29 -0400

    continued working on spiro

--- proposal2_spirograph2.py
+++ proposal2_spirograph2.py
@@ -3,10 +3,12 @@ import rhinoscriptsyntax as rs
 
 rs.DeleteObjects(rs.AllObjects('select'))#delete all existing objects
 
-def spiro(R,r,d,angle):
-    x = (R-r) * cos(angle) - d * cos(((R-r)/r)*angle) + random.randint(20,30)
-    y = (R-r) * sin(angle) - d * sin(((R-r)/r)*angle) + random.randint(20,30)
-    m.goto(x,y)
+def spiro(R,r,d,angle,moveMe):
+    x = (R-r) * cos(angle) - d * cos(((R-r)/r)*angle)
+    y = (R-r) * sin(angle) - d * sin(((R-r)/r)*angle)
+    rs.RotateObject(moveMe,basePointInner,angle)
+    pointsList.append(pen)
+
     #print('spiro')
 
 """define gears and pen sizes and locations"""
@@ -19,7 +21,9 @@ baseZ = 0
 basePointOuter = (baseX,baseY,baseZ)
 basePointInner = ((baseX + (outerGearRadius - innerGearRadius)),baseY,baseZ)
 basePointPen = ((baseX + (outerGearRadius - innerGearRadius)+ pointDistance),baseY, baseZ)
-numRotates = 100
+numRotates = 5
+angle = 15
+pointsList = []
 
 """Insure proportions are correct"""
 if outerGearRadius <= innerGearRadius or innerGearRadius <= pointDistance:
@@ -33,6 +37,12 @@ pen = rs.AddPoint(basePointPen)
 moveMe = [innerGear, pen]
 
 """Rotate the inner gear and pen"""
-"""for i in range(0,numRotates):
-    rs.MoveObject(moveMe, vector?)
-    rotation = rs.VectorCreate(startpoint, endpoint)"""
+for i in range(0,numRotates):
+    spiro(outerGearRadius,innerGearRadius,pointDistance,angle,moveMe)
+    #rs.MoveObject(moveMe, vector?)
+    #rotation = rs.VectorCreate(startpoint, endpoint)
+
+for x in range(0,len(pointsList)-1):
+    if x == len(pointsList)-1:
+            rs.AddLine(pointsList[x],pointsList[0])
+    else: rs.AddLine(pointsList[x],pointsList[x+1])
